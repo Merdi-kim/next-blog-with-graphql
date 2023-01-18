692b509b704f81bf222a4157ec4c606c5fcb80c3
Merdi-kim <merdikimuha1@gmail.com>
2023-01-18 17:15:20 +0300

    set post layout'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import type { NextPage } from 'next'
+import type { NextPage, GetStaticProps, InferGetStaticPropsType  } from 'next'
 import Head from 'next/head'
 import { PostCard, PostWidget, Categories } from '../components'
 import { getPosts } from '../services'
+import {IPost} from '../types/post'
 
-const Home: NextPage = ({posts}) => {
+
+const Home: NextPage = ({posts}:InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -12,7 +14,7 @@ const Home: NextPage = ({posts}) => {
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map(post => <PostCard/>)}
+          {posts.map((post) => <PostCard key={post.node.title} post={post.node}/>)}
         </div>
         <div className='lg:col-span-8 col-span-1'>
           <div className='lg:sticky relative top-8'>
@@ -26,9 +28,8 @@ const Home: NextPage = ({posts}) => {
   )
 }
 
-export async function getStaticProps() {
+export const getStaticProps:GetStaticProps = async() => {
   const posts = await getPosts()
-
   return {
     props: {
       posts
