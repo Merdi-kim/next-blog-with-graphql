3cdf10e91e64ef1759c684ff73344898d9ad087b
Merdi-kim <merdikimuha1@gmail.com>
2023-01-18 23:52:43 +0300

    set categories and recent posts widget

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = ({posts}:InferGetStaticPropsType<typeof getStaticProps>) 
         <div className='lg:col-span-8 col-span-1'>
           {posts.map((post) => <PostCard key={post.node.title} post={post.node}/>)}
         </div>
-        <div className='lg:col-span-8 col-span-1'>
+        <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
             <PostWidget/>
             <Categories/>
