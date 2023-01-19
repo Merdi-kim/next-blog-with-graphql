8c8e14ee2deddb2a11c64ec0d94ef10c4d24c164
Merdi-kim <merdikimuha1@gmail.com>
2023-01-19 22:43:15 +0300

    set final graphql queries

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Header } from '../components'
 import type { AppProps } from 'next/app'
 import '../styles/globals.scss'
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage, GetStaticProps, InferGetStaticPropsType  } from 'next'
 import Head from 'next/head'
 import { PostCard, PostWidget, Categories } from '../components'
 import { getPosts } from '../services'
-import {IPost} from '../types/post'
+import {IPostNode } from '../types/post'
 
 
 const Home: NextPage = ({posts}:InferGetStaticPropsType<typeof getStaticProps>) => {
@@ -14,7 +14,7 @@ const Home: NextPage = ({posts}:InferGetStaticPropsType<typeof getStaticProps>) 
       </Head>
       <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post) => <PostCard key={post.node.title} post={post.node}/>)}
+          {posts.map((post:IPostNode) => <PostCard key={post.node.title} post={post.node}/>)}
         </div>
         <div className='lg:col-span-4 col-span-1'>
           <div className='lg:sticky relative top-8'>
@@ -29,7 +29,7 @@ const Home: NextPage = ({posts}:InferGetStaticPropsType<typeof getStaticProps>) 
 }
 
 export const getStaticProps:GetStaticProps = async() => {
-  const posts = await getPosts()
+  const posts: IPostNode[] = await getPosts()
   return {
     props: {
       posts
