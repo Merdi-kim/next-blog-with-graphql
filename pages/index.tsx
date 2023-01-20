0c0e0a454f4d0a40671b068202956e72ff78de30
Merdi-kim <merdikimuha1@gmail.com>
2023-01-20 11:48:11 +0300

    set header responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,21 @@ import type { NextPage, GetStaticProps, InferGetStaticPropsType  } from 'next'
 import Head from 'next/head'
 import { PostCard, PostWidget, Categories } from '../components'
 import { getPosts } from '../services'
-import {IPostNode } from '../types/post'
+import {IPostNode } from '../types'
 
 
 const Home: NextPage = ({posts}:InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
-    <div className="container mx-auto px-10 mb-8">
+    <div className="w-full mb-8 px-6 sm:px-16">
       <Head>
         <title>merkim blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+      <div className='grid grid-cols-1 lg:grid-cols-12 gap-8'>
         <div className='lg:col-span-8 col-span-1'>
           {posts.map((post:IPostNode) => <PostCard key={post.node.title} post={post.node}/>)}
         </div>
-        <div className='lg:col-span-4 col-span-1'>
+        <div className='hidden lg:block lg:col-span-4'>
           <div className='lg:sticky relative top-8'>
             <PostWidget/>
             <Categories/>
