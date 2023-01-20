a75c21b53b32340f9b9b9ba3bef2c446ac851fb1
Merdi-kim <merdikimuha1@gmail.com>
2023-01-20 16:04:15 +0300

    remove header for testing purposes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Header } from '../components'
+//import { Header } from '../components'
 import type { AppProps } from 'next/app'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
     <>
-      <Header/>
+      {/*<Header/>*/}
       <Component {...pageProps} />
     </>
   ) 
